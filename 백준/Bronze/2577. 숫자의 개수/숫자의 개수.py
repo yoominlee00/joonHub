8e50e500be825634a49161f5bf4e9984659b9a6d
yoominlee00 <yumini606@gmail.com>
2025-03-15 11:37:31 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,7 +1,6 @@
 import sys
 a,b,c = map(int,sys.stdin.read().splitlines())
 num = [0]*10
-value = str(a*b*c)
-for i in list(value):
+for i in list(str(a*b*c)):
     num[int(i)] += 1
 list(map(print,num))
