1a32e7538877d72ac2452c769618c9cdbe7275a8
yoominlee00 <yumini606@gmail.com>
2025-03-25 16:28:26 +0900

    [Gold IV] Title: 뱀, Time: 72 ms, Memory: 35052 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -20,46 +20,9 @@ N2 = int(sys.stdin.readline())
 for _ in range(N2):
     a, b = sys.stdin.readline().split()
     dir.append((int(a),b))
-
-while True:
-    if dir:
-        t = dir[0][0]
-        s = dir[0][1]
-    a,b = snail[0]
-    if status == 'l':
-        if (b-1) < 1 or (a,b-1) in snail:
-            break
-        snail.appendleft((a,b-1))
-        if (a,b) not in app:
-            snail.pop()
-        else:
-            app.remove((a,b))
-    elif status == 'r':
-        if (b+1) > N or (a,b+1) in snail:
-            break
-        snail.appendleft((a,b+1))
-        if (a,b) not in app:
-            snail.pop()
-        else:
-            app.remove((a,b))
-    elif status == 'u':
-        if (a-1) < 1 or (a-1,b) in snail:
-            break
-        snail.appendleft((a-1,b))
-        if (a,b) not in app:
-            snail.pop()
-        else:
-            app.remove((a,b))
-    else:
-        if (a+1) > N or (a+1,b) in snail:
-            break
-        snail.appendleft((a+1,b))
-        if (a,b) not in app:
-            snail.pop()
-        else:
-            app.remove((a,b))
-    ##### 방향 전환 체크 ! ####
-    if n == t:
+########방향 전환 함수#########
+def dc(status,s):
+        global dir
         if status == 'l':
             if s == 'L':
                 status = 'd'
@@ -80,6 +43,46 @@ while True:
                 status = 'r'
             else:
                 status = 'l'
+        return status
+#############이동############
+def move(a,b,status):
+    global snail, app
+    if status == 'l':
+        if (b-1) < 1 or (a,b-1) in snail:
+            return False
+        snail.appendleft((a,b-1))
+    elif status == 'r':
+        if (b+1) > N or (a,b+1) in snail:
+            return False
+        snail.appendleft((a,b+1))
+    elif status == 'u':
+        if (a-1) < 1 or (a-1,b) in snail:
+            return False
+        snail.appendleft((a-1,b))
+    else:
+        if (a+1) > N or (a+1,b) in snail:
+            return False
+        snail.appendleft((a+1,b))
+    ### 사과 있는지 체크 ###
+    if (a,b) not in app:
+        snail.pop()
+    else:
+        app.remove((a,b))
+    return True
+
+#############메인#############
+while True:
+    if dir:
+        t = dir[0][0]
+        s = dir[0][1]
+    a,b = snail[0]
+    if not move(a,b,status):
+        break
+    #### 방향 전환 체크 ! ####
+    if n == t:
+        status = dc(status,s)
         dir.popleft()
     n += 1
+
 print(n)
+
