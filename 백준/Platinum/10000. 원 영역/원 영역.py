aba004a3777dfcbfb43eabb7c076c0e30ee8b677
yoominlee00 <yumini606@gmail.com>
2025-03-26 00:20:32 +0900

    [Platinum IV] Title: 원 영역, Time: 928 ms, Memory: 173772 KB -BaekjoonHub

--- 백준/Platinum/10000. 원 영역/원 영역.py
+++ 백준/Platinum/10000. 원 영역/원 영역.py
@@ -2,46 +2,26 @@ import sys
 N = int(sys.stdin.readline())
 l = []
 stack = []
-big = []
-
+radius = [0]*N
+cnt = 0
 for i in range(N):
     a, r = list(map(int,sys.stdin.readline().split()))
     l.append(((i,a-r,r,0))) #시작 : 0
     l.append(((i,a+r,r,1))) #끝 :1
-# l.sort(key = lambda x : (x[1],-x[3],-x[2]))
-used = [False]*N
+
 l.sort(key=lambda x: (x[1], 0 if x[3]==1 else 1, x[2] if x[3]==1 else -x[2]))
 last_type = None
 last_x = None
 
 for id, x, r, type in l:
-    if last_type is not None:
-        if last_type == 0:
-            if type == 0:
-                if x == last_x :
-                    used[stack[-1]] = True
-                    stack.append(id)
-                else:
-                    stack.append(id)
-            else:
-                stack.pop()
-        else:
-            if type == 0:
-                if x != last_x:
-                    used[stack[-1]] = False
-                    stack.append(id)
-                else:
-                    stack.append(id)
-            else:
-                if x != last_x:
-                    used[stack[-1]]=False
-                    stack.pop()
-                else:
-                    stack.pop()
-    else:
+    if type == 0:
         stack.append(id)
-    last_type = type
-    last_x =x
-
-print(N+1+sum(used))
-
+    elif type == 1:
+        if radius[id] == r:
+            cnt +=1
+        stack.pop()
+        if stack:
+            radius[stack[-1]] += r
+    
+        
+print(cnt+1+N)
