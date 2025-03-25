ab3aff1d0f4798627a2fa8f1bfc749f198ffab16
yoominlee00 <yumini606@gmail.com>
2025-03-25 10:17:11 +0900

    [Gold II] Title: 철로, Time: 424 ms, Memory: 48580 KB -BaekjoonHub

---
+++ 백준/Gold/13334. 철로/철로.py
@@ -0,0 +1,24 @@
+import sys
+import heapq
+N = int(sys.stdin.readline())
+lst = []
+train = []
+cnt = 0
+
+for _ in range(N):
+    h, o = map(int,sys.stdin.readline().split())
+    H = min(h,o)
+    O = max(h,o)
+    heapq.heappush(lst,(O,H))
+d = int(sys.stdin.readline())
+
+while lst:
+    end = heapq.heappop(lst)
+    heapq.heappush(train,end[1])
+    while train and train[0] < (end[0]-d):
+        heapq.heappop(train)
+    if len(train) >= cnt:
+        cnt = len(train)
+
+print(cnt)
+    
