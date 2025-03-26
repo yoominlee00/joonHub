f3fd4a8052d613ccd3d66e46285faf8077e3e88c
yoominlee00 <yumini606@gmail.com>
2025-03-26 14:17:12 +0900

    [Gold IV] Title: 사냥꾼, Time: 36 ms, Memory: 32412 KB, Score: 23 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,27 +1,35 @@
 import sys
-import heapq
 
 M, N, L = map(int,sys.stdin.readline().split())
 hunt = list(map(int,sys.stdin.readline().split()))
-heapq.heapify(hunt)
+hunt.sort()
 animal = []
-result = []
 cnt = 0
 
 for _ in range(N):
     x, y = map(int,sys.stdin.readline().split())
-    heapq.heappush(animal,(x+y,x-y))
-cnt = 0
+    animal.append((x,y))
 
-for i in hunt:
-    while animal and animal[0][0] <= i + L:
-        heapq.heappush(result,animal[0][1])
-        heapq.heappop(animal)
-    while result and result[0] > L - i:
-        heapq.heappop(result)
-    cnt = max(cnt,len(result))
-
+def find(pl,pr,x):
+    while pl <= pr:
+        mid = (pl+pr)//2
+        if hunt[mid] == x:
+            return mid
+        elif hunt[mid] < x:
+            return find(mid+1,pr,x)
+        else:
+            return find(pl,mid-1,x)
+    if pl >= len(hunt):
+        return pr
+    elif pr <= 0:
+        return pl
+    elif len(hunt)>1 and abs(hunt[pr]-x) >= abs(hunt[pr+1]-x):
+        return pr+1
+    else:
+        return pr
+    
+for x, y in animal:
+    hunt_id = find(0,len(hunt)-1,x)
+    if abs(hunt[hunt_id] - x) + y <= L:
+        cnt+=1
 print(cnt)
-
-
-
