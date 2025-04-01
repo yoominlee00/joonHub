bbb54e0d30fc3bcfe8bc4beb9e502c5c00477659
yoominlee00 <yumini606@gmail.com>
2025-04-01 23:32:05 +0900

    [Gold IV] Title: 빙산, Time: 4104 ms, Memory: 391876 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,53 +1,60 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
+N, M = map(int,sys.stdin.readline().split())
+graph = []
+new_graph = []
+visited = []
+q = deque()
+for _ in range(N):
+    lst = list(map(int,sys.stdin.readline().split()))
+    graph.append(lst)
+q = deque()
 
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-def check(a, b, visited, rem):
-    q = deque()
-    q.append((a, b))
-    visited[a][b] = True  # 놓치기 쉬운 부분!
-
+def check(a,b,visited,rem):
+    q.append((a,b))
+    visited[a][b] = True
     while q:
         a, b = q.popleft()
+        dx = [1,-1,0,0]
+        dy = [0,0,1,-1]
         cnt = 0
         for i in range(4):
             new_a = a + dx[i]
             new_b = b + dy[i]
-            if 0 <= new_a < N and 0 <= new_b < M:
+            if 0 <= new_a < N and 0 <= new_b < M and not visited[new_a][new_b]:
                 curr = graph[new_a][new_b]
                 if curr == 0:
                     cnt += 1
-                elif curr > 0 and not visited[new_a][new_b]:
+                elif not visited[new_a][new_b] and curr > 0:
                     visited[new_a][new_b] = True
-                    q.append((new_a, new_b))
+                    q.append((new_a,new_b))
         rem[a][b] += cnt
+area = 0
 
 def ice(n):
     rem = [[0]*M for _ in range(N)]
-    visited = [[False]*M for _ in range(N)]
     area = 0
+    visited = []
+    for _ in range(N):
+        visited.append([False]*M)
 
     for i in range(N):
         for j in range(M):
-            if graph[i][j] > 0 and not visited[i][j]:
-                area += 1
-                check(i, j, visited, rem)
-
+            if graph[i][j] > 0:
+                if not visited[i][j]:
+                    area +=1
+                    check(i,j,visited,rem)
+    ##
     if area == 0:
         return 0
     if area >= 2:
         return n
-
+    
     for i in range(N):
         for j in range(M):
-            graph[i][j] = max(0, graph[i][j] - rem[i][j])  # 빼고 음수 방지
-
-    return ice(n + 1)
+            graph[i][j] -= rem[i][j] 
+            if graph[i][j] < 0 :
+                graph[i][j] = 0
+    return ice(n+1)
 
 print(ice(0))
