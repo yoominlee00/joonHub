77b7aeb7554327a2061eb5657f892f9453bd9cbb
yoominlee00 <yumini606@gmail.com>
2025-04-03 11:00:13 +0900

    [Silver IV] Title: 바닥 장식, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -6,6 +6,19 @@ for _ in range(N):
     graph.append(list(sys.stdin.readline().strip()))
 visited = [[False]*M for _ in range(N)]
 cnt = 0
+
+# 가로/ 세로 체크 함수
+def ck(x,y):
+    if graph[x][y] == '-':
+        if y+1 < M:
+            if graph[x][y+1] == '-':
+                return (x,y+1)
+    elif graph[x][y] == '|':
+        if x+1 < N:
+            if graph[x+1][y] == '|':
+                return (x+1,y)
+    return
+
 for i in range(N):
     for j in range(M):
         if not visited[i][j]:
@@ -16,13 +29,7 @@ for i in range(N):
             while q:
                 x,y = q.popleft()
                 visited[x][y] = True
-                if graph[x][y] == '-':
-                    if y+1 < M:
-                        if graph[x][y+1] == '-':
-                            q.append((x,y+1))
-                elif graph[x][y] == '|':
-                    if x+1 < N:
-                        if graph[x+1][y] == '|':
-                            q.append((x+1,y))
+                if ck(x,y):
+                    q.append(ck(x,y))
 print(cnt)
 
