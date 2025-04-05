c89468255e9a24d6b54e3fe0915dcc2fe6ade42f
yoominlee00 <yumini606@gmail.com>
2025-04-05 15:34:55 +0900

    [level 3] Title: 단어 변환, Time: 0.80 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,37 @@
+import sys
+sys.setrecursionlimit(10**6)
+def solution(begin, target, words):
+    global min_n
+    visited = [False]*(len(words))
+    result = []
+    min_n = float('inf')
+    def dfs(idx,n):
+        global min_n
+        if n != 0 and words[idx] == target:
+            min_n = min(min_n,n)
+            return 
+        
+        if n >= len(words):
+            min_n = 0
+            return
+            
+        if n == 0:
+            start = idx
+        else:
+            start = words[idx]
+        lst = []
+        for i,val in enumerate(words):
+            cnt = 0
+            for j in range(len(val)):
+                if start[j] != val[j]:
+                    cnt +=1
+            if cnt == 1 and not visited[i]:
+                lst.append(i)
+        # index를 lst에 넣는다.
+        for i in lst:
+            visited[i] = True
+            dfs(i,n+1)
+            visited[i] = False
+        return 
+    dfs(begin,0)
+    return 0 if min_n == float('inf') else min_n
