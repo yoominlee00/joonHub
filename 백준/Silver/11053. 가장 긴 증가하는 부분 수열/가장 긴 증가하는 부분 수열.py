2833e317ed4efcccaf6bc51f346c5ecaa97f489d
yoominlee00 <yumini606@gmail.com>
2025-04-09 16:32:54 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,19 +1,15 @@
 import sys
-import bisect
-from collections import deque
-
-N = int(sys.stdin.readline())
+N = int(input())
 lst = list(map(int,sys.stdin.readline().split()))
-lst = deque(lst)
-stack = deque()
+# i 번째 수를 마지막으로 하는 LIS 길이 (자기자신 하나 기본값으로 저장)
+dp = [1]*N
 
-while lst:
-    a = lst[0]
-    index = bisect.bisect_left(stack,a)
-    if index < len(stack):
-        stack[index] = a 
-    else:
-        stack.append(a)
-    lst.popleft()
-print(len(stack))
-    
+#순차탐색
+for i in range(N):
+    #탐색한 범위까지 또 비교
+    for j in range(i):
+        # 자기보다 작은 게 있으면 그때까지 누적한 값 + 자기자신인 1
+        # 그 중 가장 긴 수열 채택 ! 
+        if lst[j] < lst[i]:
+            dp[i] = max(dp[i],dp[j]+1)
+print(max(dp))
