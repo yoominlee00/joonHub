d81bb4fcabf32bafeb26fa5edad58d6a49fc566f
yoominlee00 <yumini606@gmail.com>
2025-06-21 15:25:21 +0900

    [Silver III] Title: 타노스, Time: 92 ms, Memory: 108384 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,4 +1,25 @@
+import sys
+
 lst =  list(map(int,input()))
-num_1 = sum(lst)
-num_0 = len(lst)-num_1
-print('0'*(num_0//2)+'1'*(num_1//2))
+num_1 = sum(lst)//2
+num_0 = (len(lst)-num_1*2)//2
+cnt_1 = 0
+cnt_0 = 0 
+result = []
+
+
+for i in lst:
+    if i == 1:
+        if cnt_1 < num_1:
+            cnt_1 += 1
+        else:
+            result.append('1')
+            cnt_1 += 1
+    else:
+        if cnt_0 < num_0:
+            result.append('0')
+            cnt_0 += 1
+        else:
+            cnt_0 += 1
+
+print(''.join(result))
